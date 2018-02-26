0dfadd7b81a438e5b7f13798686db6c1a8c0a863
tuckbarb <36681088+tuckbarb@users.noreply.github.com>
2018-02-26 17:27:30 -0500

    moar
    +local resources
    +auto height slides

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class Slide extends React.Component {
 
   constructor(props){
     super(props);
-    var sh = window.innerHeight+100;
+    var sh = window.innerHeight;
     this.state = { height:sh };
   }
 
@@ -44,15 +44,50 @@ class Slide extends React.Component {
   }
 }
 
+class Mountains extends React.Component {
+
+  constructor(props){
+    super(props);
+    var sh = window.innerHeight;
+    this.state = { height:sh };
+  }
+
+  resizeHeight(){
+    this.setState( { height:window.innerHeight } );
+  }
+
+  componentDidMount() {
+       window.addEventListener("resize", () => this.resizeHeight() );
+   }
+
+   componentWillUnmount() {
+       window.removeEventListener("resize", () => this.resizeHeight() );
+   }
+
+  render() {
+
+
+    var style = {
+      height : this.state.height+"px"
+    }
+
+    return (
+      <div class="mountains" style={style}>
+        <div id="dry"></div>
+        <div id="snowy"></div>
+      </div>
+    );
+  }
+}
+
 class Portfolio extends React.Component {
   render() {
 
 
     return (
       <div>
-        <Slide id="landing-page" greeting="woo land" />
-        <Slide id="snow-page" greeting="woo snow" />
-        <Slide />
+        <Slide id="landing-page" greeting={'woo\n slide 1'} />
+        <Slide id="snow-page" greeting={'woo\n slide 2'} />
       </div>
     );
   }
